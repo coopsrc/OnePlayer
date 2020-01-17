b651ae46306ed74ba49fd773b2a41130c8b34473
Zhang Tingkuo <zhangtingkuo@gmail.com>
2020-01-17 16:48:24 +0800

    Update gradle config.
    Signed-off-by: Zhang Tingkuo <zhangtingkuo@gmail.com>

--- player/base/src/main/java/com/coopsrc/oneplayer/core/utils/PlayerLogger.java
+++ player/base/src/main/java/com/coopsrc/oneplayer/core/utils/PlayerLogger.java
@@ -2,7 +2,7 @@ package com.coopsrc.oneplayer.core.utils;
 
 import androidx.annotation.NonNull;
 
-import timber.log.Timber;
+import com.coopsrc.xandroid.utils.LogUtils;
 
 /**
  * @author tingkuo
@@ -15,58 +15,52 @@ public class PlayerLogger {
         throw new UnsupportedOperationException(this.getClass().getSimpleName() + " cannot be instantiated");
     }
 
-    static {
-        Timber.tag(Constants.LOG_TAG);
-        Timber.plant(new Timber.DebugTree());
-//        Timber.plant(new LogTree());
-    }
-
     public static void i(String message, Object... args) {
-        Timber.tag(Constants.LOG_TAG).i(message, args);
+        LogUtils.tag(Constants.LOG_TAG).i(message, args);
     }
 
     public static void d(String message, Object... args) {
-        Timber.tag(Constants.LOG_TAG).d(message, args);
+        LogUtils.tag(Constants.LOG_TAG).d(message, args);
     }
 
     public static void w(String message, Object... args) {
-        Timber.tag(Constants.LOG_TAG).w(message, args);
+        LogUtils.tag(Constants.LOG_TAG).w(message, args);
     }
 
     public static void e(String message, Object... args) {
-        Timber.tag(Constants.LOG_TAG).e(message, args);
+        LogUtils.tag(Constants.LOG_TAG).e(message, args);
     }
 
     public static void v(String message, Object... args) {
-        Timber.tag(Constants.LOG_TAG).v(message, args);
+        LogUtils.tag(Constants.LOG_TAG).v(message, args);
     }
 
     public static void wtf(String message, Object... args) {
-        Timber.tag(Constants.LOG_TAG).wtf(message, args);
+        LogUtils.tag(Constants.LOG_TAG).wtf(message, args);
     }
 
     public static void i(@NonNull String tag, String message, Object... args) {
-        Timber.tag(tag).i(message, args);
+        LogUtils.tag(tag).i(message, args);
     }
 
     public static void d(@NonNull String tag, String message, Object... args) {
-        Timber.tag(tag).d(message, args);
+        LogUtils.tag(tag).d(message, args);
     }
 
     public static void w(@NonNull String tag, String message, Object... args) {
-        Timber.tag(tag).w(message, args);
+        LogUtils.tag(tag).w(message, args);
     }
 
     public static void e(@NonNull String tag, String message, Object... args) {
-        Timber.tag(tag).e(message, args);
+        LogUtils.tag(tag).e(message, args);
     }
 
     public static void v(@NonNull String tag, String message, Object... args) {
-        Timber.tag(tag).v(message, args);
+        LogUtils.tag(tag).v(message, args);
     }
 
     public static void wtf(@NonNull String tag, String message, Object... args) {
-        Timber.tag(tag).wtf(message, args);
+        LogUtils.tag(tag).wtf(message, args);
     }
 
 }
